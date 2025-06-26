7f2c84536b6d16c86f526d91810fd83155c9e979
Fergus Maclean <fergus.smaclean@gmail.com>
2025-06-26 15:53:21 +0100

    added prompt for player to input choice

--- javascript.js
+++ javascript.js
@@ -14,4 +14,21 @@ function getComputerChoice(){
 
 console.log(getComputerChoice());
 
+const signButton = document.querySelector("#signButton");
+const log = document.querySelector("#log");
+
+signButton.addEventListener("click", () => {
+  let sign = prompt("Rock, Paper, Scissors?");
+
+  if (sign === null) {
+    log.innerText = "OK, maybe next time.";
+  } else if (sign.toLowerCase() === "") {
+    log.innerText = "Enter your choice!";
+  } else if (sign.toLowerCase() === "scorpio") {
+    log.innerText = "Wow! I'm a Scorpio too!";
+  } else {
+    log.innerText = `${sign}`;
+  }
+});
+
 
